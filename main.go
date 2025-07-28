d2ca83c01ba2de4bc7d0efb5dc3227330a58a583
Pandy_Yi <95898667+panyichen48737@users.noreply.github.com>
2025-07-29 04:22:12 +0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ var (
 	dataPath     = flag.String("datapath", filepath.Join("./", "data"), "Path to your custom 'data' directory")
 	datName      = flag.String("datname", "geosite.dat", "Name of the generated dat file")
 	outputPath   = flag.String("outputpath", "./publish", "Output path to the generated files")
-	exportLists  = flag.String("exportlists", "private,microsoft,apple,google,category-games,netflix,disney,hbo,primevideo,apple-tvplus,youtube,tiktok,bilibili,spotify,category-ai-!cn,speedtest,tld-!cn,geolocation-!cn,cn", "Lists to be exported in plaintext format, separated by ',' comma")
+	exportLists  = flag.String("exportlists", "private,microsoft,apple,google,category-games,netflix,disney,hbo,primevideo,apple-tvplus,youtube,tiktok,bilibili,spotify,category-ai-!cn,speedtest,tld-!cn,tld-cn,geolocation-!cn,cn", "Lists to be exported in plaintext format, separated by ',' comma")
 	excludeAttrs = flag.String("excludeattrs", "private@ads,microsoft@ads,apple@ads,google@ads,category-games@ads,netflix@ads,disney@ads,hbo@ads,primevideo@ads,apple-tvplus@ads,youtube@ads,tiktok@ads,bilibili@ads,spotify@ads,category-ai-!cn@ads,speedtest@ads,tld-!cn@cn@ads,geolocation-!cn@cn@ads,cn@!cn@ads", "Exclude rules with certain attributes in certain lists, seperated by ',' comma, support multiple attributes in one list. Example: geolocation-!cn@cn@ads,geolocation-cn@!cn")
 )
 
